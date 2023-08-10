85781e4cc10e03011ce58ae9495bdeeac6526709
Caleb Ukle <caleb.ukle+github@pm.me>
2023-08-10 15:38:24 -0500

    vite

--- apps/v-demo-two/src/app/nx-welcome.cy.tsx
+++ apps/v-demo-two/src/app/nx-welcome.cy.tsx
@@ -3,6 +3,7 @@ import NxWelcome from './nx-welcome';
 
 describe(NxWelcome.name, () => {
   it('renders', () => {
-    cy.mount(<NxWelcome />);
+    cy.mount(<NxWelcome title="v-demo-two cy" />);
+    cy.get('h1').should('contain.text', 'Welcome v-demo-two cy');
   });
 });
